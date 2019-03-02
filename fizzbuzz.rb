86a19ce12576feb721cdf3c0680f43a70948d628
favorable-vector-1743 <epzads@gmail.com>
2019-03-02 16:03:01 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,23 +4,25 @@
 
 
 def fizzbuzz(number)
-  
-  if (number % 3 == 0) && (number % 5 == 0)
-    
-    return "FizzBuzz"
-    
-  else if
-    (number % 3 == 0) && (number % 5 != 0)
-    return "Fizz"
-    
-  else if 
-    (number % 5 == 0) && (number % 3 != 0)
-    return "Fizz"
-    
-  else 
-    
-    return nil 
-  end 
-  end   
-  end 
+ 
+ if (number % 3 == 0) && (number % 5 == 0)
+   
+   return "FizzBuzz"
+   
+ else if
+   (number % 3 == 0) && (number % 5 != 0)
+   return "Fizz"
+   
+ else if 
+   (number % 5 == 0) && (number % 3 != 0)
+   return "Buzz"
+   
+ else 
+   
+   return "nil" 
+ 
+ end   
+ end 
+end 
+
 end 
